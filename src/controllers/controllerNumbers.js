404c7edb5931681fd88663aab44aa23a05d66563
gabriel1325t <160737281+gabriel1325t@users.noreply.github.com>
2024-09-18 13:00:32 -0300

    controllerNumbers.js

--- src/controllers/controllerNumbers.js
+++ src/controllers/controllerNumbers.js
@@ -2,9 +2,14 @@ module.exports = class controllerNumbers{
     static async checkPar(req, res){
         const {numero} = req.body;
 
+        if(typeof numero !=="number"){
+            return res.status(400).json({message:"Não é numero"})
+
+    }
+
         if (numero % 2 == 0){
             return res.status(200).json({message: "Número Par!"});
-        }
+        }else{
         return res.status(200).json({message: "Numero impar!"});
     }
     static async checkPrimo(req, res){
